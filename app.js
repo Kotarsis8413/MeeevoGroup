25ead74867f3ce3f1eceb5996383e5e67307ea13
kotarsis8413 <kotarsis8413@bk.ru>
2024-05-19 14:47:16 +0300

    mgsitev1

---
+++ app.js
@@ -0,0 +1,40 @@
+const app = document.getElementById("info");
+
+const mainpage = (
+    <div>
+    <h3 className="center">Главная</h3>
+    <span className="m10">MeeevoGroup - небольшая команда разработчиков-любителей, которой нечем заняться. 
+    Именно по этому мы можем предложить порой никому не нужные проекты, но которые могут быть интересными.</span>
+    </div>
+);
+
+const contacts = (
+    <div>
+    <h3 className="center">Контакты</h3>
+    <span className="m10">Не уверены, что кому-то захочется связаться с нами, но если хотите: 
+    kotarsis8413@bk.ru - почта
+    </span>
+    </div>
+)
+
+const projects = (
+    <div>
+    <h3 className="center">Проекты</h3>
+    <span className="m10">Пока что мы не успели ничего сделать кроме этого сайта, если интересно, то он написан на React js
+    </span>
+    </div>
+)
+
+ReactDOM.render(mainpage, app);
+
+function setToMain() {
+    ReactDOM.render(mainpage, app);
+}
+
+function setToContacts() {
+    ReactDOM.render(contacts, app);
+}
+
+function setToProjects() {
+    ReactDOM.render(projects, app);
+}
