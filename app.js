4e36a6641e3a934f14d00d7c7a2d23354c306d66
Кот <93428046+Kotarsis8413@users.noreply.github.com>
2024-05-19 15:43:56 +0300

    app.js update

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ const root = ReactDOM.createRoot(appM);
 
 
 const pages = {
-    mainpage = (
+    mainpage: (
         <div>
         <h3 className="center">Главная</h3>
         <span className="m10">MeeevoGroup - небольшая команда разработчиков-любителей, которой нечем заняться. 
         Именно по этому мы можем предложить порой никому не нужные проекты, но которые могут быть интересными.</span>
         </div>
     ),
-    contacts = (
+    contacts: (
         <div>
         <h3 className="center">Контакты</h3>
         <span className="m10">Не уверены, что кому-то захочется связаться с нами, но если хотите: 
@@ -22,7 +22,7 @@ const pages = {
         </span>
         </div>
     ),
-    projects = (
+    projects: (
         <div>
         <h3 className="center">Проекты</h3>
         <span className="m10">Пока что мы не успели ничего сделать кроме этого сайта, если интересно, то он написан на React js
